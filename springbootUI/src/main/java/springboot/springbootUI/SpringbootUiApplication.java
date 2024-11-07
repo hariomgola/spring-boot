ec861d5d6c9d7050ed86dc4f7e71298f673c3d5f
hariom <hariom987316@gmail.com>
2024-11-07 16:16:19 +0530

    [Adding SpringbootUI]

---
+++ springbootUI/src/main/java/springboot/springbootUI/SpringbootUiApplication.java
@@ -0,0 +1,13 @@
+package springboot.springbootUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUiApplication.class, args);
+	}
+
+}
---
+++ springbootUI/src/test/java/springboot/springbootUI/SpringbootUiApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.springbootUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
