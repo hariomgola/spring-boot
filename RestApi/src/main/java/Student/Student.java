ed1d11ce9e2922ac5e881c02d12f44cd83cc1618
hariom <hariom987316@gmail.com>
2025-02-14 12:55:59 +0530

    [Updating lambok]

--- RestApi/src/main/java/Student/Student.java
+++ RestApi/src/main/java/Student/Student.java
@@ -9,7 +9,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class Student {
 	@Id
@@ -22,60 +32,4 @@ public class Student {
 	@OneToMany(mappedBy = "student") // here the name of property in post student property
 	@JsonIgnore
 	private List<Post> post;
-
-	public Student() {
-	}
-
-	public Student(int id, String name, String message, String address) {
-		this.id = id;
-		this.name = name;
-		this.message = message;
-		this.address = address;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public List<Post> getPost() {
-		return post;
-	}
-
-	public void setPost(List<Post> post) {
-		this.post = post;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", message=" + message + ", address=" + address + "]";
-	}
-
 }
