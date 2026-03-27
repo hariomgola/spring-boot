c14e9a7095ddb35654e73d540a491fa61eb217dc
Hari <hariom987316@gmail.com>
2026-03-27 09:02:31 +0530

    [Updating project structure]

---
+++ h2InMemory/src/main/java/in/memroy/db/h2InMemory/H2InMemoryApplication.java
@@ -0,0 +1,13 @@
+package in.memroy.db.h2InMemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2InMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2InMemoryApplication.class, args);
+	}
+
+}
---
+++ h2InMemory/src/test/java/in/memroy/db/h2InMemory/H2InMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package in.memroy.db.h2InMemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2InMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
